a0effaf01b6a8cfb1570ea96768bad81b74ec649
Daniel Farrelly <daniel@todaysplan.com.au>
2021-07-19 14:25:41 +1000

    Don't need .failedDecodingResponse as an underlying error for .failedDecodingResponse

--- Zone5/Transport/Zone5HTTPClient.swift
+++ Zone5/Transport/Zone5HTTPClient.swift
@@ -311,7 +311,7 @@ extension JSONDecoder {
 				// special handling required for Void types. Enforce enpty data. Create NoReply object.
 				if data.count > 0 {
 					z5Log("Failed to decode server response as `\(expectedType)`. Error: Non Void response")
-					return .failure(.failedDecodingResponse(Zone5.Error.failedDecodingResponse(Zone5.Error.unknown)))
+					return .failure(.failedDecodingResponse(Zone5.Error.unknown))
 				} else {
 					return .success(Zone5.VoidReply() as! T)
 				}
