3f048b645cab6e32376cd751b2adfa0eda546e69
Charlie Bartel <charles.bartel@specialized.com>
2023-10-06 10:59:46 -0600

    [TACO-4712] User can Authorize Komoot in Connected Apps (#65)
    * Add Komoot support
    
    * fix spacing
    
    * clean up
    
    * bump version to 41

--- Zone5/Enums/UserConnectionType.swift
+++ Zone5/Enums/UserConnectionType.swift
@@ -7,61 +7,48 @@
 //
 
 public enum UserConnectionType: Int, Codable, CaseIterable {
+    case garminconnect = 0
+    case fitbit = 1
+    case withings = 2
+    case myfitnesspal = 3
+    case underarmour = 4
+    case garminwellness = 5
+    case trainingpeaks = 6
+    case strava = 7
+    case polar = 8
+    case ridewithgps = 9
+    case todaysplan = 10
+    case suunto = 11
+    case garmintraining = 12
+    case turbo = 13
+    // Specialized Ride
+    case specialized = 14
+    case nike = 15
+    case wahoo = 16
+    case whoop = 17
+    case komoot = 18
 
-	case garminconnect = 0
-	
-	case fitbit = 1
-	
-	case withings = 2
-
-	case myfitnesspal = 3
-	
-	case underarmour = 4
-	
-	case garminwellness = 5
-	
-	case trainingpeaks = 6
-	
-	case strava = 7
-	
-	case polar = 8
-	
-	case ridewithgps = 9
-	
-	case todaysplan = 10
-	
-	case suunto = 11
-	
-	case garmintraining = 12
-	
-	case turbo = 13
-	
-	// Specialized Ride
-	case specialized = 14
-	
-	case nike = 15
-	
-	case wahoo = 16
-	
-	var connectionName: String {
-		switch self {
-		case .garminconnect: return "garminconnect"
-		case .fitbit: return "fitbit"
-		case .withings: return "withings"
-		case .myfitnesspal: return "myfitnesspal"
-		case .underarmour: return "underarmour"
-		case .garminwellness: return "garminwellness"
-		case .trainingpeaks: return "trainingpeaks"
-		case .strava: return "strava"
-		case .polar: return "polar"
-		case .ridewithgps: return "ridewithgps"
-		case .todaysplan: return "todaysplan"
-		case .suunto: return "suunto"
-		case .garmintraining: return "garmintraining"
-		case .turbo: return "turbo"
-		case .specialized: return "specialized"
-		case .nike: return "nike"
-		case .wahoo: return "wahoo"
-		}
-	}
+    public var connectionName: String {
+        switch self {
+        case .garminconnect: return "garminconnect"
+        case .fitbit: return "fitbit"
+        case .withings: return "withings"
+        case .myfitnesspal: return "myfitnesspal"
+        case .underarmour: return "underarmour"
+        case .garminwellness: return "garminwellness"
+        case .trainingpeaks: return "trainingpeaks"
+        case .strava: return "strava"
+        case .polar: return "polar"
+        case .ridewithgps: return "ridewithgps"
+        case .todaysplan: return "todaysplan"
+        case .suunto: return "suunto"
+        case .garmintraining: return "garmintraining"
+        case .turbo: return "turbo"
+        case .specialized: return "specialized"
+        case .nike: return "nike"
+        case .wahoo: return "wahoo"
+        case .whoop: return "whoop"
+        case .komoot: return "komoot"
+        }
+    }
 }
